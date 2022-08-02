1db04ea14e3be3e2044f439b01da7f308b5ae54c
minhvan <minhvan.996@outlook.com>
2022-08-03 01:06:08 +0700

    FEAT: Init Server

--- DirectoryMonitorSystem/src/main/app-server/ServerUI.java
+++ DirectoryMonitorSystem/src/main/app-server/ServerUI.java
@@ -1,2 +1,27 @@
-public class ServerUI {
+import javax.swing.*;
+
+public class ServerUI extends JFrame{
+    private JPanel mainPanel;
+    private JPanel headerPanel;
+    private JPanel bodyPanel;
+    private JPanel footerPanel;
+    private JPanel clientsListPanel;
+    private JPanel clientLogPanel;
+    private JTable clientsListTable;
+    private JScrollPane clientsListScrollPanel;
+    private JLabel welcomeLabel;
+    private JLabel clientsListLabel;
+    private JLabel clientLogLabel;
+
+    public ServerUI(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new ServerUI("Directory Monitor Server");
+        frame.setVisible(true);
+    }
 }

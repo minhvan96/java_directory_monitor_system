1f6e14c03721eacdd610f38d6eba9d50ba25130b
minhvan <minhvan.996@outlook.com>
2022-08-07 18:17:42 +0700

    FEAT: Add client UI

--- DirectoryMonitorSystem/src/main/app-client/ClientUI.java
+++ DirectoryMonitorSystem/src/main/app-client/ClientUI.java
@@ -1,12 +1,23 @@
 import javax.swing.*;
 
-public class ClientUI {
+public class ClientUI extends JFrame{
     private JPanel mainPanel;
     private JPanel headerPanel;
     private JPanel bodyPanel;
     private JPanel footerPanel;
+    private JLabel serverAddressLabel;
+    private JTextField serverAddressTextField;
+    private JButton connectToServerButton;
+
+    public ClientUI(String title) {
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+    }
 
     public static void main(String[] args){
-
+        JFrame frame = new ClientUI("Client");
+        frame.setVisible(true);
     }
 }
